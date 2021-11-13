bece46d729a0a9ce920c6a57780a82b75bd52d2f
Pedro Verani <pedropverani@hotmail.com>
2021-11-13 18:26:15 -0300

    adding required fields in compliment

--- src/models/compliment-model.js
+++ src/models/compliment-model.js
@@ -4,11 +4,11 @@ const Schema = Mongoose.Schema;
 
 const complimentSchema = new Schema({
     createdAt: {type: Date, default: Date.now()},
-    fromId: {type: String},
-    fromName: {type: String},
-    toId: {type: String},
-    toName: {type: String},
-    content: {type: String},
+    fromId: {type: String, required: true},
+    fromName: {type: String, required: true},
+    toId: {type: String, required: true},
+    toName: {type: String, required: true},
+    content: {type: String, required: true},
 })
 
 module.exports = Mongoose.model('complimentSchema', complimentSchema);
