d5fbd7bdd14b8d048b60f3232816d0f887d39fae
Ahgora <pedropverani@hotmail.com>
2021-11-13 22:23:41 -0300

    adding validation

--- src/presentation/routers/compliment-router.js
+++ src/presentation/routers/compliment-router.js
@@ -1,11 +1,12 @@
 const router = require('express').Router();
 const complimentController = require('../../controllers/compliment-controller');
+const { validateRequiredFieldInCompliment } = require('../middlewares/helper/validator');
 
 router.get('/', complimentController.getAll);
 
 router.get('/:id', complimentController.getAllByEmployeeId);
 
-router.post('/', complimentController.create);
+router.post('/', validateRequiredFieldInCompliment, complimentController.create);
 
 router.patch('/:id/reaction', complimentController.updateReaction);
 
